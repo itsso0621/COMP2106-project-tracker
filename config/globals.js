bf5e7158304960e657f3d5171d61920aba6954f6
SoYoung Lee <soyounglee@SoYoungs-MacBook-Pro.local>
2021-02-18 14:52:20 -0500

    lesson week 5 - mongoDB

---
+++ config/globals.js
@@ -0,0 +1,5 @@
+// store any global variables and export so they are public
+module.exports = {
+  db:
+    "mongodb+srv://SoYoung100:dudthdl6077@cluster0.wuja4.mongodb.net/comp2106",
+};
--- controllers/projects.js
+++ controllers/projects.js
@@ -2,9 +2,47 @@
 const express = require("express");
 const router = express.Router();
 
+// add project model for CRUD operations
+const Project = require("../models/project");
+
 /** GET /projects */
 router.get("/", (req, res, next) => {
-  res.render("projects/index", { title: "My Projects" });
+  Project.find((err, projects) => {
+    if (err) {
+      console.log(err);
+    } else {
+      // Load the index view, set the title, and pass the query resulttest as "projects"
+      res.render("projects/index", {
+        title: "My Projects",
+        projects: projects,
+      });
+    }
+  });
+});
+
+/** GET /projects/add */
+router.get("/add", (req, res, next) => {
+  res.render("projects/add", { title: "Project Details" });
+});
+
+/** POST /projects/add */
+router.post("/add", (req, res, next) => {
+  // use the Project model to save the form data to MongoDB
+  Project.create(
+    {
+      name: req.body.name,
+      dueDate: req.body.dueDate,
+      course: req.body.course,
+    },
+    (err, newProject) => {
+      if (err) {
+        console.log(err);
+      } else {
+        // if successful, redirect to projects index
+        res.redirect("/projects");
+      }
+    }
+  );
 });
 
 //make public
